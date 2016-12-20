387ea2c4507baa4a2b64415e8dfac443e54b4131
Sébastien Mischler (aka skarab) <skarab@bluewin.ch>
2016-12-20 08:57:15 +0100

    0.1.2

--- dist/lw.canvas-filters.js
+++ dist/lw.canvas-filters.js
@@ -1,2 +1,257 @@
-!function(e,a){"object"==typeof exports&&"object"==typeof module?module.exports=a():"function"==typeof define&&define.amd?define("CanvasFilter",[],a):"object"==typeof exports?exports.CanvasFilter=a():e.CanvasFilter=a()}(this,function(){return function(e){function a(o){if(t[o])return t[o].exports;var n=t[o]={exports:{},id:o,loaded:!1};return e[o].call(n.exports,n,n.exports,a),n.loaded=!0,n.exports}var t={};return a.m=e,a.c=t,a.p="",a(0)}([function(e,a,t){e.exports=t(1)},function(e,a){"use strict";function t(e){return e<0?0:e>255?255:e}function o(e,a,o){void 0!==o&&(e[a]=t(e[a]+o),e[a+1]=t(e[a+1]+o),e[a+2]=t(e[a+2]+o))}function n(e,a,o){void 0!==o&&(e[a]=t(o*(e[a]-128)+128),e[a+1]=t(o*(e[a+1]-128)+128),e[a+2]=t(o*(e[a+2]-128)+128))}function r(e,a,o){void 0!==o&&(e[a]=t(Math.exp(Math.log(255*(e[a]/255))*o)),e[a+1]=t(Math.exp(Math.log(255*(e[a+1]/255))*o)),e[a+2]=t(Math.exp(Math.log(255*(e[a+2]/255))*o)))}function i(e,a,o,n){if(m.indexOf(o)===-1)throw new Error("Unsupported grayscale algorithm: "+o);if("none"===o)return null;var r=void 0,i=e[a],s=e[a+1],c=e[a+2];switch(o){case"average":r=(i+s+c)/3;break;case"luma":r=.3*i+.59*s+.11*c;break;case"luma-601":r=.299*i+.587*s+.114*c;break;case"luma-709":r=.2126*i+.7152*s+.0722*c;break;case"luma-240":r=.212*i+.701*s+.087*c;break;case"desaturation":r=(Math.max(i,s,c)+Math.min(i,s,c))/2;break;case"decomposition-min":r=Math.min(i,s,c);break;case"decomposition-max":r=Math.max(i,s,c);break;case"red-chanel":r=i;break;case"green-chanel":r=s;break;case"blue-chanel":r=c}void 0!==n&&(r=parseInt(r/n)*n),r=parseInt(r),e[a]=t(r),e[a+1]=t(r),e[a+2]=t(r)}function s(e,a){a=Object.assign({},{smoothing:!1,brightness:0,contrast:0,gamma:0,grayscale:"none",shadesOfGray:256},a||{});var t=e.getContext("2d");void 0!==t.imageSmoothingEnabled?t.imageSmoothingEnabled=a.smoothing:(t.mozImageSmoothingEnabled=a.smoothing,t.webkitImageSmoothingEnabled=a.smoothing,t.msImageSmoothingEnabled=a.smoothing,t.oImageSmoothingEnabled=a.smoothing);var s=t.getImageData(0,0,e.width,e.height),m=s.data,c=void 0,d=void 0,l=void 0,h=void 0;0!==a.contrast&&(c=259*(a.contrast+255)/(255*(259-a.contrast))),0!==a.brightness&&(d=a.brightness),0!==a.gamma&&(l=1/a.gamma),a.shadesOfGray>1&&a.shadesOfGray<256&&(h=255/(a.shadesOfGray-1));for(var g=0,u=m.length;g<u;g+=4)o(m,g,d),n(m,g,c),r(m,g,l),i(m,g,a.grayscale,h);t.putImageData(s,0,0)}Object.defineProperty(a,"__esModule",{value:!0});var m=["none","average","desaturation","decomposition-min","decomposition-max","luma","luma-601","luma-709","luma-240","red-chanel","green-chanel","blue-chanel"];a.canvasFilters=s,a.default=s}])});
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define("CanvasFilter", [], factory);
+	else if(typeof exports === 'object')
+		exports["CanvasFilter"] = factory();
+	else
+		root["CanvasFilter"] = factory();
+})(this, function() {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__(1);
+
+
+/***/ },
+/* 1 */
+/***/ function(module, exports) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	    value: true
+	});
+	// Grayscale algorithms
+	var grayscaleAlgorithms = ['none', 'average', 'desaturation', 'decomposition-min', 'decomposition-max', 'luma', 'luma-601', 'luma-709', 'luma-240', 'red-chanel', 'green-chanel', 'blue-chanel'];
+	
+	// Trucate color value in the 0-255 range
+	function color(color) {
+	    return color < 0 ? 0 : color > 255 ? 255 : color;
+	}
+	
+	// Filters ...
+	function brightness(data, i, value) {
+	    if (value !== undefined) {
+	        data[i] = color(data[i] + value);
+	        data[i + 1] = color(data[i + 1] + value);
+	        data[i + 2] = color(data[i + 2] + value);
+	    }
+	}
+	
+	function contrast(data, i, value) {
+	    if (value !== undefined) {
+	        data[i] = color(value * (data[i] - 128) + 128);
+	        data[i + 1] = color(value * (data[i + 1] - 128) + 128);
+	        data[i + 2] = color(value * (data[i + 2] - 128) + 128);
+	    }
+	}
+	
+	function gamma(data, i, value) {
+	    if (value !== undefined) {
+	        data[i] = color(Math.exp(Math.log(255 * (data[i] / 255)) * value));
+	        data[i + 1] = color(Math.exp(Math.log(255 * (data[i + 1] / 255)) * value));
+	        data[i + 2] = color(Math.exp(Math.log(255 * (data[i + 2] / 255)) * value));
+	    }
+	}
+	
+	function grayscale(data, i, algorithm, shades) {
+	    // Graysale
+	    // http://www.tannerhelland.com/3643/grayscale-image-algorithm-vb6/
+	
+	    // Unsupported algorithm
+	    if (grayscaleAlgorithms.indexOf(algorithm) === -1) {
+	        throw new Error('Unsupported grayscale algorithm: ' + algorithm);
+	    }
+	
+	    // None
+	    if (algorithm === 'none') {
+	        return null;
+	    }
+	
+	    // Get Red/Green/Blue values
+	    var gray = void 0;
+	    var r = data[i];
+	    var g = data[i + 1];
+	    var b = data[i + 2];
+	
+	    switch (algorithm) {
+	        case 'average':
+	            gray = (r + g + b) / 3;
+	            break;
+	
+	        case 'luma':
+	            // Default
+	            gray = r * 0.3 + g * 0.59 + b * 0.11;
+	            break;
+	
+	        case 'luma-601':
+	            // CCIR-601
+	            gray = r * 0.299 + g * 0.587 + b * 0.114;
+	            break;
+	
+	        case 'luma-709':
+	            // ITU-R-709
+	            gray = r * 0.2126 + g * 0.7152 + b * 0.0722;
+	            break;
+	
+	        case 'luma-240':
+	            // SMPTE-240M
+	            gray = r * 0.212 + g * 0.701 + b * 0.087;
+	            break;
+	
+	        case 'desaturation':
+	            gray = (Math.max(r, g, b) + Math.min(r, g, b)) / 2;
+	            break;
+	
+	        case 'decomposition-min':
+	            gray = Math.min(r, g, b);
+	            break;
+	
+	        case 'decomposition-max':
+	            gray = Math.max(r, g, b);
+	            break;
+	
+	        case 'red-chanel':
+	            gray = r;
+	            break;
+	
+	        case 'green-chanel':
+	            gray = g;
+	            break;
+	
+	        case 'blue-chanel':
+	            gray = b;
+	            break;
+	    }
+	
+	    // Shades of gray
+	    if (shades !== undefined) {
+	        gray = parseInt(gray / shades) * shades;
+	    }
+	
+	    // Force integer
+	    gray = parseInt(gray);
+	
+	    // Set new r/g/b values
+	    data[i] = color(gray);
+	    data[i + 1] = color(gray);
+	    data[i + 2] = color(gray);
+	}
+	
+	// Apply filters on provided canvas
+	function canvasFilters(canvas, settings) {
+	    settings = Object.assign({}, {
+	        smoothing: false, // Smoothing [true|fale]
+	        brightness: 0, // Image brightness [-255 to +255]
+	        contrast: 0, // Image contrast [-255 to +255]
+	        gamma: 0, // Image gamma correction [0.01 to 7.99]
+	        grayscale: 'none', // Graysale algorithm [average, luma, luma-601, luma-709, luma-240, desaturation, decomposition-[min|max], [red|green|blue]-chanel]
+	        shadesOfGray: 256 // Number of shades of gray [2-256]
+	    }, settings || {});
+	
+	    // Get canvas 2d context
+	    var context = canvas.getContext('2d');
+	
+	    // Smoothing
+	    if (context.imageSmoothingEnabled !== undefined) {
+	        context.imageSmoothingEnabled = settings.smoothing;
+	    } else {
+	        context.mozImageSmoothingEnabled = settings.smoothing;
+	        context.webkitImageSmoothingEnabled = settings.smoothing;
+	        context.msImageSmoothingEnabled = settings.smoothing;
+	        context.oImageSmoothingEnabled = settings.smoothing;
+	    }
+	
+	    // Get image data
+	    var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+	    var data = imageData.data;
+	
+	    var contrastFactor = void 0,
+	        brightnessOffset = void 0,
+	        gammaCorrection = void 0,
+	        shadesOfGrayFactor = void 0;
+	
+	    if (settings.contrast !== 0) {
+	        contrastFactor = 259 * (settings.contrast + 255) / (255 * (259 - settings.contrast));
+	    }
+	
+	    if (settings.brightness !== 0) {
+	        brightnessOffset = settings.brightness;
+	    }
+	
+	    if (settings.gamma !== 0) {
+	        gammaCorrection = 1 / settings.gamma;
+	    }
+	
+	    // Shades of gray
+	    if (settings.shadesOfGray > 1 && settings.shadesOfGray < 256) {
+	        shadesOfGrayFactor = 255 / (settings.shadesOfGray - 1);
+	    }
+	
+	    // For each pixel
+	    for (var i = 0, il = data.length; i < il; i += 4) {
+	        // Apply filters
+	        brightness(data, i, brightnessOffset);
+	        contrast(data, i, contrastFactor);
+	        gamma(data, i, gammaCorrection);
+	        grayscale(data, i, settings.grayscale, shadesOfGrayFactor);
+	    }
+	
+	    // Write new image data on the context
+	    context.putImageData(imageData, 0, 0);
+	}
+	
+	// Exports
+	exports.canvasFilters = canvasFilters;
+	exports.default = canvasFilters;
+
+/***/ }
+/******/ ])
+});
+;
 //# sourceMappingURL=lw.canvas-filters.js.map
